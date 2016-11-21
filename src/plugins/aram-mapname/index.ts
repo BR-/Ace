8bb0690dcace1d620aa6dbd32e66516c73e59d38
BR- <wishingwellwellwishing@hotmail.com>
2016-11-20 23:08:06 -0800

    maybe this will get us something useful idk

--- src/plugins/aram-mapname/index.ts
+++ src/plugins/aram-mapname/index.ts
@@ -1,6 +1,5 @@
 "use strict";
 
-import Ace from  "../../ace";
 import { PluginDescription } from "../../plugin";
 
 import newHTML = require("./aram.html");
@@ -10,20 +9,26 @@ export default (<PluginDescription>{
     version: "1.0.0",
     description: "Renames the 'ARAM' map to 'Howling Abyss' to match the others.",
     builtinDependencies: {
-        "rcp-fe-lol-parties": "~0.0.598-any"
+        "rcp-fe-lol-parties": "~0.0.598-any",
+        "rcp-fe-l10n": "*"
     },
     setup() {
+        let l10n: any;
+        this.postinit("rcp-fe-l10n", p => {
+            l10n = p.api;
+        });
         this.preinit("rcp-fe-lol-parties", () => {
             let unregister = this.hook("ember-component", Ember => {
                 unregister();
-                return Mixin(Ember, this.ace);
+                return Mixin(Ember, l10n);
             }, "parties-game-type-select-wrapper");
         });
     }
 });
 
-const Mixin = (Ember: any, ace: Ace) => ({
+const Mixin = (Ember: any, l10n: any) => ({
     didInsertElement() {
+        (<any>window)['asd'] = l10n;
         this._super();
         Ember.run.scheduleOnce('afterRender', this, function() {
             const mapname = this.$(".map-12 > .parties-game-type-card-name")[0];
